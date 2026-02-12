6a52ee36e19f080ec46a35e442bbb6a807274cf6
Quentin <quentin.delsol@patrim.fr>
2026-02-12 11:34:32 +0100

    Fix: Correction erreur TypeScript loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import SplashScreen from "@/components/SplashScreen";
 
 export default function Loading() {
-  return <SplashScreen />;
+  // On passe une fonction vide () => null car Next.js gère lui-même la fin du chargement ici
+  return <SplashScreen onComplete={() => null} />;
 }
